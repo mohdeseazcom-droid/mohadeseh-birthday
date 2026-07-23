b6cacd26bdda8d4430619f27d1483adc3fb1819a
mohdeseazcom-droid <mohdeseaz.com@gmail.com>
2026-07-23 23:45:12 +0330

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/save", methods=["POST"])
+def save():
+    name = request.form["name"]
+    cake = request.form["cake"]
+    wish = request.form["wish"]
+    birth = request.form["birth"]
+
+    with open("users.txt", "a", encoding="utf-8") as file:
+        file.write(
+            f"اسم: {name}\n"
+            f"تاریخ تولد: {birth}\n"
+            f"کیک مورد علاقه: {cake}\n"
+            f"آرزو: {wish}\n"
+            "------------------\n"
+        )
+
+    return "اطلاعات ثبت شد 🌸"
+
+
+app.run(host="0.0.0.0", port=5000)
